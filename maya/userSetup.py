bc8c41efbc7dda4ba2a8317a83153792fce3f259
Jenya Desheviy <jenya.renderman@gmail.com>
2020-12-28 17:32:29 +0200

    maya 2020 plugins

--- maya/userSetup.py
+++ maya/userSetup.py
@@ -30,12 +30,12 @@ import soundImport
 
 _MODULE_LIST = ['assetDailies', 'previzToMontage', 'camera_publisher', 'zshotmask', 'setShotInOut', 'soundImport']  #
 
-# try:
-#     ppath = os.getenv("MAYA_PLUG_IN_PATH")
-#     ppath += ";" + "X:/app/win/Pipeline/maya/plugins"
-#     os.environ["MAYA_PLUG_IN_PATH"] = ppath
-# except:
-#     os.environ["MAYA_PLUG_IN_PATH"] = "X:/app/win/Pipeline/maya/plugins"
+try:
+    ppath = os.getenv("MAYA_PLUG_IN_PATH")
+    ppath += ";" + "X:/app/win/Pipeline/maya/plugins"
+    os.environ["MAYA_PLUG_IN_PATH"] = ppath
+except:
+    os.environ["MAYA_PLUG_IN_PATH"] = "X:/app/win/Pipeline/maya/plugins"
 
 
 def createUI():
