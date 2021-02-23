de981c99f916eb175a59fea822f49d45c09860bf
Alexander Gamaiunov <gamaiunchik@gmail.com>
2021-02-23 12:07:05 +0200

    light aovs to rops edited 2

--- houdini_app/sashokScripts/light_aovs_to_rop.py
+++ houdini_app/sashokScripts/light_aovs_to_rop.py
@@ -1,14 +1,13 @@
 import hou
 
 def main():
-    print 'in main latr'
+    #print 'in main latr'
 
     nn = hou.selectedNodes()
-    print 'sel nodes %s' % str(nn)
+    #print 'sel nodes %s' % str(nn)
     contrib = ['RGBA_' + i.parm('ar_aov').eval() for i in nn]
     contrib = list(set(contrib))
-    print 'cccc ' + str(contrib)
-    return
+    #print 'cccc ' + str(contrib)
 
     out = hou.node('/out')
     obj = hou.node('/obj')
@@ -18,7 +17,7 @@ def main():
     ropToAdjust = hou.ui.selectFromList([i.name() for i in rr if i.type().name() == 'arnold'])
     for i in ropToAdjust:
         r = rr[i]
-        print r.name()
+        #print r.name()
         #contrib_local = contrib
         aovs_amount = int(r.parm('ar_aovs').eval())
         aovs = [r.parm('ar_aov_label%s'%str(i+1)).eval() for i in range(aovs_amount)]
