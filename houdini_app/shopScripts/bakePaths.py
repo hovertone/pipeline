b41a25f1e907e6026ef8b59fc1b7accc456c7689
Alexander Gamaiunov <gamaiunchik@gmail.com>
2021-08-11 12:42:07 +0300

    TP color family and space references

--- houdini_app/shopScripts/bakePaths.py
+++ houdini_app/shopScripts/bakePaths.py
@@ -1,12 +1,19 @@
-#print '\n\n\n================== in bake paths ======================'
-shopnet = hou.pwd().parent()
-for s in shopnet.children():
-    print s.name()
-    for i in s.children():
-        if i.type().name() == 'arnold::image':
-            p = i.parm('filename')
-            # LINKED PARMS BAKING
-            val = p.eval()
-            p.revertToDefaults()
-            p.set(val)
-            print "\t%s's filename parameter has been baked" % i.name()
+print '\n\n\n================== in bake paths ======================'
+import hou
+try:
+    shopnet = hou.pwd().parent()
+    parms_to_bake = ['filename', 'color_family', 'color_space']
+    for s in shopnet.children():
+        print s.name()
+        for i in s.children():
+            if i.type().name() == 'arnold::image':
+                for ptb in parms_to_bake:
+                    print '\t\t:::: baking %s type' % ptb
+                    p = i.parm(ptb)
+                    # LINKED PARMS BAKING
+                    val = p.eval()
+                    p.revertToDefaults()
+                    p.set(val)
+                    print "\t%s's filename parameter has been baked" % i.name()
+except:
+    pass
